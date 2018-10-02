696e870d123b80ed672b735de15617d60ba14667
Allanbrito <allandbc1202@gmail.com>
2018-10-02 16:58:16 -0600

    jh

---
+++ Dummy.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class Dummy here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Dummy
+{
+    
+    public Dummy()
+    {
+       
+    }
+
+   
+}
